1de3646218a9cd2ad588a0d38031f0e5eea5daa3
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-05-28 21:58:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ class Driver {
 
   trips () {
     return store.trips.filter(function(trip) {
-      return this.driverId === driver.id
+      return this.id === trip.driverId
     }.bind(this))
   }
 
