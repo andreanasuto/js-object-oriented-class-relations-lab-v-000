df2a7eb25eefaa190a1c5c1def8728270c7739fa
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-05-28 21:15:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,10 @@ class Passenger {
 }
 
 class Trip {
-  constructor() {
+  constructor(driver, passenger) {
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
     store.trips.push(this)
   }
 }
